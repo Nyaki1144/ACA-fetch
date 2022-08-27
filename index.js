f426d8b88a1ad8b947b16fe873f0c4ea29350882
Narek <narek1144@gmail.com>
2022-08-27 17:47:41 +0400

    fix: fix logic

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const btn = document.querySelector(".btn");
-const imgWrapper = document.querySelector(".img-wrapper");
 
 btn.addEventListener("click", () => {
+  const imgWrapper = document.querySelector(".img-wrapper");
   imgWrapper.innerHTML = `<img class="img" src="./39.gif" alt="img" />`;
 
   const lists = document.querySelector(".lists");
 
   fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((el) => el.json())
+    .then((el) => {
+      imgWrapper.remove();
+      return el.json();
+    })
     .then((el) =>
       el.forEach((element) => {
         const li = document.createElement("li");
@@ -22,7 +25,6 @@ btn.addEventListener("click", () => {
                     `;
 
         lists.append(li);
-        imgWrapper.remove();
       })
     );
 });
