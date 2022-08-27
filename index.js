3d1b093f05981fba617bbdf367388fb1ad904b8f
Narek <narek1144@gmail.com>
2022-08-27 18:35:24 +0400

    fix: fix logic agein

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", () => {
-  const imgWrapper = document.querySelector(".img-wrapper");
-  imgWrapper.innerHTML = `<img class="img" src="./39.gif" alt="img" />`;
-
   const lists = document.querySelector(".lists");
+
+  lists.innerHTML = `<img class="img" src="./39.gif" alt="img"/>`;
 
   fetch("https://jsonplaceholder.typicode.com/posts")
     .then((el) => {
-      imgWrapper.remove();
       return el.json();
     })
-    .then((el) =>
+    .then((el) => {
+      lists.firstChild.remove();
+
       el.forEach((element) => {
         const li = document.createElement("li");
 
         li.classList.add("list");
 
         li.innerHTML = `
-                        <div class="padd userId">${element.userId}</div>
-                        <div class="padd id">${element.id}</div>
-                        <div class="color title">${element.title}</div>
-                        <div class=" body">${element.body}</div>
-                    `;
+                            <div class="padd userId">${element.userId}</div>
+                            <div class="padd id">${element.id}</div>
+                            <div class="color title">${element.title}</div>
+                            <div class=" body">${element.body}</div>
+                        `;
 
         lists.append(li);
-      })
-    );
+      });
+    });
 });
